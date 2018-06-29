ad89637018d13d50a0aea66a4b7e6cc93a24b836
varnell-holdings <john.lamia@gmail.com>
2018-06-29 15:46:08 +1000

    second version iconifies

--- watcher.py
+++ watcher.py
@@ -4,6 +4,8 @@ Created on Mon Jun 25 13:27:05 2018
 
 @author: John
 """
+
+import time
 import webbrowser
 from tkinter import *
 from tkinter import ttk
@@ -15,9 +17,13 @@ observer = Observer()
 
 class Handler(PatternMatchingEventHandler):
     def on_modified(self, event: FileModifiedEvent):
-        global lab
+        global lab, root, but
         lab.configure(text='New Patient!')
         lab.configure(background='red')
+#        root.attributes("-topmost", True)
+        time.sleep(20)
+        root.deiconify()
+        root.attributes("-topmost", True)
 
 observer.schedule(event_handler=Handler('*'), path='d:\\john tillet\\episode_data\\csv\\')
 observer.daemon=False
@@ -30,8 +36,12 @@ observer.start()
 
 def runner(*args):
     lab.configure(text='Up to date!')
-    lab.configure(background='Green')
+    lab.configure(background='green')
+#    webbrowser.register('chrome', None)
     webbrowser.open('d:\\Nobue\\today_new.html')
+#    root.attributes("-topmost", False)
+    time.sleep(2)
+    root.iconify()
     
 
 
@@ -51,11 +61,12 @@ lab = ttk.Label(mainframe, text="Up to date!")
 lab.grid(column=0, row=0, sticky=W)
 
 
-but = ttk.Button(mainframe, text='Open web.', command=runner)
+but = ttk.Button(mainframe, text='Open web page', command=runner)
 but.grid(column=0, row=1, sticky=E)
+
 
 for child in mainframe.winfo_children():
     child.grid_configure(padx=5, pady=5)
 
-root.attributes("-topmost", True)
+
 root.mainloop()

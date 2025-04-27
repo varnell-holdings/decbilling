375464fdd6a0ab25f972ebc1d2f8fd1dc32cde81
varnell-holdings <john.lamia@gmail.com>
2025-04-27 14:17:51 +1000

    add headers to episodes.csv

--- csv_modify.py
+++ csv_modify.py
@@ -1,6 +1,15 @@
 import csv
+import pandas as pd
 
-with open("day_surgery.csv") as f1, open("episodes.csv", "w") as f2:
+with open("day_surgery.csv") as f1, open("episodes_ext.csv", "w") as f2:
+    reader = csv.reader(f1)
+    writer = csv.writer(f2)
+    for a in reader:
+        if len(a) == 14:
+            a.extend(["", "", "", "", "", ""])
+        writer.writerow(a)
+
+with open("episodes_ext.csv") as f1, open("episodes_intermediate.csv", "w") as f2:
     reader = csv.reader(f1)
     writer = csv.writer(f2)
     for a in reader:
@@ -16,3 +25,37 @@ with open("day_surgery.csv") as f1, open("episodes.csv", "w") as f2:
         a[12] = ""
         a[13] = ""
         writer.writerow(a)
+
+
+# Read the CSV file without headers
+df = pd.read_csv("episodes_intermediate.csv", header=None)
+
+# Define your headers
+headers = [
+    "date",
+    "mrn",
+    "in",
+    "out",
+    "anaes",
+    "endo",
+    "asa",
+    "upper",
+    "lower",
+    "anal",
+    "nurse",
+    "clips",
+    "free1",
+    "free2",
+    "caecum",
+    "title",
+    "firstname",
+    "surname",
+    "dob",
+    "email",
+]
+
+# Assign the headers to the dataframe
+df.columns = headers
+
+# Save the file with headers
+df.to_csv("episodes.csv", index=False)
